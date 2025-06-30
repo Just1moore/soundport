1244ac3e1b695fd2ce1c56ba66f3f1681bb0f3a7
Just1moore <omondicliff09@outlook.com>
2025-06-30 16:58:25 -0500

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
-app.use(express.static);   //  ← this passes `undefined`, so “root path must be a string”
+app.use(express.static(__dirname));
 
 
 
